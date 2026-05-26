f74b499f8397618f86eb774060b0c44e16bbe341
fmahmud <fmahmud@perfectserve.com>
2026-05-26 09:08:48 -0700

    Console OTEL sample

---
+++ OtelConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OtelConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
